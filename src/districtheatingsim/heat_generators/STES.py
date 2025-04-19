storage calculation fixes

src/districtheatingsim/heat_generators/STES.py
@@ -675,7 +675,8 @@ class TemperatureStratifiedThermalStorage(StratifiedThermalStorage):
         self.Q_net_storage_flow = np.zeros(self.hours)
 
         self.T_max_rücklauf = 70  # Maximale Rücklauftemperatur für Erzeuger
-        self.T_min_vorlauf = 70  # Minimale Vorlauftemperatur für Verbraucher
+
+        self.dT_VLT = 10  # Tolerance below the target supply temperature in K
 
     def simulate_stratified_temperature_mass_flows(self, t, Q_in, Q_out, T_Q_in_flow, T_Q_out_return):
         """
@@ -689,11 +690,9 @@ class TemperatureStratifiedThermalStorage(StratifiedThermalStorage):
 
         T_Q_in_flow_copy = np.copy(T_Q_in_flow)
         T_Q_out_return_copy = np.copy(T_Q_out_return)
-        Q_in_copy = np.copy(Q_in)
-        Q_out_copy = np.copy(Q_out)
 
-        self.Q_in = Q_in_copy  # Input heat in kW
-        self.Q_out = Q_out_copy  # Output heat in kW
+        self.Q_in = np.zeros(self.hours)  # Eingangsleistung in kW
+        self.Q_out = np.zeros(self.hours)  # Ausgangsleistung in kW
         self.T_Q_in_flow = T_Q_in_flow_copy  # Vorlauf-Erzeuger
         self.T_Q_out_return = T_Q_out_return_copy  # Rücklauf-Verbraucher
         
@@ -731,30 +730,30 @@ class TemperatureStratifiedThermalStorage(StratifiedThermalStorage):
             # when the storage is full, the inflow is zero
             if self.T_sto_layers[t, -1] < self.T_max_rücklauf:
                 # Ladeoperation möglich
-                self.mass_flow_in[t] = (Q_in * 1000) / (self.cp * (T_Q_in_flow - self.T_sto_layers[t, -1]))
+                self.Q_in[t] = Q_in
+                self.mass_flow_in[t] = (self.Q_in[t] * 1000) / (self.cp * (T_Q_in_flow - self.T_sto_layers[t, -1]))
             else:
                 # Speicherüberhitzung oder keine ausreichende Temperaturdifferenz
                 self.mass_flow_in[t] = 0
                 self.excess_heat += Q_in
                 self.stagnation_time += 1
-                self.Q_in = 0
+                self.Q_in[t] = Q_out
 
             # when the storage is empty, the outflow is zero
-            if self.T_sto_layers[t, 0] > self.T_min_vorlauf:
+            if self.T_sto_layers[t, 0] > T_Q_in_flow - self.dT_VLT:
                 # Entladeoperation möglich
-                self.mass_flow_out[t] = (Q_out * 1000) / (self.cp * (self.T_sto_layers[t, 0] - T_Q_out_return))
+                self.Q_out[t] = Q_out
+                self.mass_flow_out[t] = (self.Q_out[t] * 1000) / (self.cp * (self.T_sto_layers[t, 0] - T_Q_out_return))
             else:
                 # Speicher zu kalt oder keine ausreichende Temperaturdifferenz
                 self.mass_flow_out[t] = 0
                 self.unmet_demand += Q_out
-                self.Q_out = 0
-            
+                self.Q_out[t] = Q_in
 
-            #self.mass_flow_in[t] = (Q_in * 1000) / (self.cp * (T_Q_in_flow - self.T_sto_layers[t, -1]))
-            #self.mass_flow_out[t] = (Q_out * 1000) / (self.cp * (self.T_sto_layers[t, 0] - T_Q_out_return))
+            self.Q_net_storage_flow[t] = self.Q_out[t] - self.Q_in[t]
 
-            # Berechne den Netto-Wärmefluss als Differenz zwischen ein- und ausströmender Wärmemenge
-            self.Q_net_storage_flow[t] = Q_out - Q_in
+            if t == 100:
+                print(f"Mass flow in: {self.mass_flow_in[t]} kg/s, Mass flow out: {self.mass_flow_out[t]} kg/s, Q_in: {self.Q_in[t]}, Q_out: {self.Q_out[t]}, Q_net: {self.Q_net_storage_flow[t]}")
 
             # Inflow from top to bottom
             for i in range(self.num_layers):
@@ -871,8 +870,8 @@ class TemperatureStratifiedThermalStorage(StratifiedThermalStorage):
         axs6 = fig.add_subplot(2, 3, 6, projection='3d')
 
         # Separate positive and negative values for Q_net_storage_flow
-        Q_net_positive = np.maximum(self.Q_net_storage_flow, 0)  # Charging (positive values)
-        Q_net_negative = np.minimum(self.Q_net_storage_flow, 0)  # Discharging (negative values)
+        Q_net_positive = np.maximum(self.Q_net_storage_flow, 0)  # Discharging (positive values)
+        Q_net_negative = np.minimum(self.Q_net_storage_flow, 0)  # Charging (negative values)
 
         # Plot Wärmeerzeugung as line plot
         axs1.plot(Q_out, label='Wärmeverbrauch', color='blue', linewidth=0.5)

src/districtheatingsim/heat_generators/energy_system.py
@@ -178,28 +178,32 @@ class EnergySystem:
 
             for t in range(time_steps):
                 Q_in_total = 0  # Summe der Wärmeeinspeisung
-                Q_out = self.load_profile[t] # Wärmebedarf
+
                 T_Q_in_flow = self.VLT_L[t] # Vorlauftemperatur
                 T_Q_out_return = self.RLT_L[t] # Rücklauftemperatur
-                remaining_demand = Q_out
+
+                Q_out_total = self.load_profile[t] # Wärmebedarf
+                remaining_load = Q_out_total
 
                 # Speicherzustand und Temperaturen abrufen
                 upper_storage_temp, lower_storage_temp = self.storage.current_storage_temperatures(t)
 
                 # Generatoren basierend auf Priorität steuern
                 for i, tech in enumerate(self.technologies):
-                    tech.active = tech.strategy.decide_operation(tech.active, upper_storage_temp, lower_storage_temp, remaining_demand)
+                    tech.active = tech.strategy.decide_operation(tech.active, upper_storage_temp, lower_storage_temp, remaining_load)
 
                     if tech.active:
-                        Q_in, _ = tech.generate(t, remaining_demand)
-                        remaining_demand -= Q_in
+                        Q_in, _ = tech.generate(t, remaining_load)
+                        remaining_load -= Q_in
                         Q_in_total += Q_in
 
                 # Speicher aktualisieren
-                self.storage.simulate_stratified_temperature_mass_flows(t, Q_in_total, Q_out, T_Q_in_flow, T_Q_out_return)
+                self.storage.simulate_stratified_temperature_mass_flows(t, Q_in_total, Q_out_total, T_Q_in_flow, T_Q_out_return)
 
-                # Restlast nach Speicherentladung berechnen
-                remaining_demand -= self.storage.Q_net_storage_flow[t]
+                if t == 100:
+                    print(f"Speicherzustand: {self.storage_state[t]}")
+                    print(f"Speicherbeladung: {self.storage.Q_net_storage_flow[t]}")
+                    print(f"Restlast: {remaining_load}")
 
             # Speicherergebnisse berechnen
             self.storage.calculate_efficiency(self.load_profile)
@@ -263,6 +267,7 @@ class EnergySystem:
         if self.storage:
             storage_results = self.results['storage_class']
             Q_net_storage_flow = storage_results.Q_net_storage_flow
+            print(f"Speicherergebnisse: {Q_net_storage_flow[8000]}")
 
             # Speicherbeladung (negative Werte) und Speicherentladung (positive Werte) trennen
             Q_net_positive = np.maximum(Q_net_storage_flow, 0)  # Speicherentladung
@@ -308,29 +313,52 @@ class EnergySystem:
         # Füge die restlichen Variablen hinzu
         stackplot_vars += [var for var in selected_vars if var not in stackplot_vars and "_Wärmeleistung" in var]
 
+        # Speicherentladung ans Ende verschieben
+        if "Speicherentladung_kW" in stackplot_vars:
+            stackplot_vars.remove("Speicherentladung_kW")
+            stackplot_vars.append("Speicherentladung_kW")
+
         # Linienvariablen (z. B. Lastprofil)
         line_vars = [var for var in selected_vars if var not in stackplot_vars and var != "Last_L"]
 
-        # Konvertiere time_steps in Stunden (float)
-        time_steps_in_hours = self.results["time_steps"].astype('timedelta64[h]').astype(float)
-
         # Stackplot-Daten vorbereiten
-        stackplot_data = [self.extracted_data[var] for var in stackplot_vars if var in self.extracted_data]
+        stackplot_data = []
+        for var in stackplot_vars:
+            if var == "Speicherbeladung_kW" and var in self.extracted_data:
+                # Speicherbeladung als negative Werte darstellen, aber nicht in den Stackplot integrieren
+                ax1.fill_between(
+                    self.results["time_steps"],
+                    0,
+                    self.extracted_data[var],
+                    label=var,
+                    step="mid",
+                    color="gray",
+                    alpha=1.0,
+                )
+            elif var in self.extracted_data:
+                stackplot_data.append(self.extracted_data[var])
+
+        # Zeichne den Stackplot für die restlichen Variablen
         if stackplot_data:
-            ax1.stackplot(time_steps_in_hours, stackplot_data, labels=stackplot_vars)
+            ax1.stackplot(
+                self.results["time_steps"],
+                stackplot_data,
+                labels=[var for var in stackplot_vars if var != "Speicherbeladung_kW"],
+                step="mid"
+            )
 
         # Linienplot
         ax2 = ax1.twinx() if second_y_axis else None
         for var_name in line_vars:
             if var_name in self.extracted_data:
                 if ax2:
-                    ax2.plot(time_steps_in_hours, self.extracted_data[var_name], label=var_name)
+                    ax2.plot(self.results["time_steps"], self.extracted_data[var_name], label=var_name)
                 else:
-                    ax1.plot(time_steps_in_hours, self.extracted_data[var_name], label=var_name)
+                    ax1.plot(self.results["time_steps"], self.extracted_data[var_name], label=var_name)
 
         # Lastprofil
         if "Last_L" in selected_vars:
-            ax1.plot(time_steps_in_hours, self.results["Last_L"], color='blue', label='Last', linewidth=0.5)
+            ax1.plot(self.results["time_steps"], self.results["Last_L"], color='blue', label='Last', linewidth=0.25)
 
         # Achsentitel und Legende
         ax1.set_title("Jahresganglinie")