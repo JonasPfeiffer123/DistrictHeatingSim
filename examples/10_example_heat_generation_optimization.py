Fixed examples 08, 09, 10 which weren't working due to data structure changes. Also fixed secondary producer calculation in pandapipes, which came up during example testing. Recalculated some of the example results

examples/10_example_heat_generation_optimization.py
@@ -1,23 +1,24 @@
 """
 Filename: 10_example_heat_generation_optimization.py
 Author: Dipl.-Ing. (FH) Jonas Pfeiffer
-Date: 2024-11-19
+Date: 2025-07-12
 Description: Example for the optimization of a heat generator mix
 
-# Not up-to-date: This script is not up-to-date with the latest version of all heat generator classes.
+Updated to match the latest version of all heat generator classes.
 """
 
-from districtheatingsim.heat_generators import solar_thermal
-from districtheatingsim.heat_generators import gas_boiler
-from districtheatingsim.heat_generators import biomass_boiler
-from districtheatingsim.heat_generators import chp
-from districtheatingsim.heat_generators import base_heat_pumps
-from districtheatingsim.heat_generators import power_to_heat
-from districtheatingsim.heat_generators import energy_system
+from districtheatingsim.heat_generators.solar_thermal import SolarThermal
+from districtheatingsim.heat_generators.gas_boiler import GasBoiler
+from districtheatingsim.heat_generators.biomass_boiler import BiomassBoiler
+from districtheatingsim.heat_generators.chp import CHP
+from districtheatingsim.heat_generators.waste_heat_pump import WasteHeatPump
+from districtheatingsim.heat_generators.river_heat_pump import RiverHeatPump
+from districtheatingsim.heat_generators.geothermal_heat_pump import Geothermal
+from districtheatingsim.heat_generators.power_to_heat import PowerToHeat
+from districtheatingsim.heat_generators.energy_system import EnergySystem
 from districtheatingsim.utilities.test_reference_year import import_TRY
 
 import numpy as np
-
 import matplotlib.pyplot as plt
 
 def test_berechnung_erzeugermix(optimize=False, plot=True, opt_method="SLSQP"):
@@ -33,9 +34,9 @@ def test_berechnung_erzeugermix(optimize=False, plot=True, opt_method="SLSQP"):
     print(Last_L)
 
     # Wirtschaftliche Randbedingungen
-    electricity_price = 200 # €/MWh
+    electricity_price = 150 # €/MWh - Updated to match 09 example
     gas_price = 70 # €/MWh
-    wood_price = 40 # €/MWh
+    wood_price = 60 # €/MWh - Updated to match 09 example
 
     q = 1.05
     r = 1.03
@@ -50,122 +51,294 @@ def test_berechnung_erzeugermix(optimize=False, plot=True, opt_method="SLSQP"):
     COP_data = np.genfromtxt("examples/data/COP/Kennlinien WP.csv", delimiter=';')
 
     # Dateiname Testreferenzjahr für Wetterdaten, Dateiname muss ggf. angepasst werden
-    TRY_data = import_TRY("examples/data/TRY/TRY_511676144222/TRY2015_511676144222_Jahr.dat")  
-    gBoiler = gas_boiler.GasBoiler(name="Gaskessel_1", spez_Investitionskosten=30, Nutzungsgrad=0.9, Faktor_Dimensionierung=1)
+    TRY_data = import_TRY("examples/data/TRY/TRY_511676144222/TRY2015_511676144222_Jahr.dat")
 
-    PTH = power_to_heat.PowerToHeat(name="Power-to-Heat_1", spez_Investitionskosten=30, Nutzungsgrad=0.9, Faktor_Dimensionierung=1)
+    # UPDATED: Technology definitions matching 09 example
+    gBoiler = GasBoiler(
+        name="Gas_Boiler_1", 
+        thermal_capacity_kW=1000,  # Updated parameter name
+        spez_Investitionskosten=30, 
+        Nutzungsgrad=0.9
+    )
 
-    bBoiler = biomass_boiler.BiomassBoiler(name="Biomassekessel_1", thermal_capacity_kW=200, Größe_Holzlager=40, spez_Investitionskosten=200, 
-                                           spez_Investitionskosten_Holzlager=400, Nutzungsgrad_BMK=0.8, min_Teillast=0.3, speicher_aktiv=False, 
-                                           Speicher_Volumen=20, T_vorlauf=90, T_ruecklauf=60, initial_fill=0.0, min_fill=0.2, max_fill=0.8, 
-                                           spez_Investitionskosten_Speicher=750, active=True, opt_BMK_min=0, opt_BMK_max=1000, opt_Speicher_min=0, 
-                                           opt_Speicher_max=100)
+    PTH = PowerToHeat(
+        name="PowerToHeat_1", 
+        thermal_capacity_kW=1000,  # Updated parameter name
+        spez_Investitionskosten=30, 
+        Nutzungsgrad=0.98  # Updated efficiency
+    )
 
-    bBoiler_storage = biomass_boiler.BiomassBoiler(name="Biomassekessel_2", thermal_capacity_kW=200, Größe_Holzlager=40, spez_Investitionskosten=200, 
-                                                   spez_Investitionskosten_Holzlager=400, Nutzungsgrad_BMK=0.8, min_Teillast=0.3, speicher_aktiv=True, 
-                                                   Speicher_Volumen=20, T_vorlauf=90, T_ruecklauf=60, initial_fill=0.0, min_fill=0.2, max_fill=0.8, 
-                                                   spez_Investitionskosten_Speicher=750, active=True, opt_BMK_min=0, opt_BMK_max=1000, opt_Speicher_min=0, 
-                                                   opt_Speicher_max=100)
+    bBoiler = BiomassBoiler(
+        name="Biomass_Boiler_1", 
+        thermal_capacity_kW=200, 
+        Größe_Holzlager=40, 
+        spez_Investitionskosten=200, 
+        spez_Investitionskosten_Holzlager=400, 
+        Nutzungsgrad_BMK=0.8, 
+        min_Teillast=0.3, 
+        speicher_aktiv=False, 
+        Speicher_Volumen=20, 
+        T_vorlauf=90, 
+        T_ruecklauf=60, 
+        initial_fill=0.0, 
+        min_fill=0.2, 
+        max_fill=0.8, 
+        spez_Investitionskosten_Speicher=750, 
+        active=True, 
+        opt_BMK_min=0, 
+        opt_BMK_max=1000, 
+        opt_Speicher_min=0, 
+        opt_Speicher_max=100
+    )
 
-    CHP = chp.CHP(name="BHKW_1", th_Leistung_BHKW=100, spez_Investitionskosten_GBHKW=1500, spez_Investitionskosten_HBHKW=1850, el_Wirkungsgrad=0.33, 
-                  KWK_Wirkungsgrad=0.9, min_Teillast=0.7, speicher_aktiv=False, Speicher_Volumen_BHKW=20, T_vorlauf=90, T_ruecklauf=60, initial_fill=0.0, 
-                  min_fill=0.2, max_fill=0.8, spez_Investitionskosten_Speicher=750, active=True, opt_BHKW_min=0, opt_BHKW_max=1000, opt_BHKW_Speicher_min=0, 
-                  opt_BHKW_Speicher_max=100)    
+    bBoiler_storage = BiomassBoiler(
+        name="Biomass_Boiler_2", 
+        thermal_capacity_kW=200, 
+        Größe_Holzlager=40, 
+        spez_Investitionskosten=200, 
+        spez_Investitionskosten_Holzlager=400, 
+        Nutzungsgrad_BMK=0.8, 
+        min_Teillast=0.3, 
+        speicher_aktiv=True,  # Updated: with storage
+        Speicher_Volumen=20, 
+        T_vorlauf=90, 
+        T_ruecklauf=60, 
+        initial_fill=0.0, 
+        min_fill=0.2, 
+        max_fill=0.8, 
+        spez_Investitionskosten_Speicher=750, 
+        active=True, 
+        opt_BMK_min=0, 
+        opt_BMK_max=1000, 
+        opt_Speicher_min=0, 
+        opt_Speicher_max=100
+    )
 
-    CHP_storage = chp.CHP(name="BHKW_2", th_Leistung_BHKW=100, spez_Investitionskosten_GBHKW=1500, spez_Investitionskosten_HBHKW=1850, el_Wirkungsgrad=0.33, 
-                          KWK_Wirkungsgrad=0.9, min_Teillast=0.7, speicher_aktiv=True, Speicher_Volumen_BHKW=20, T_vorlauf=90, T_ruecklauf=60, initial_fill=0.0, 
-                          min_fill=0.2, max_fill=0.8, spez_Investitionskosten_Speicher=750, active=True, opt_BHKW_min=0, opt_BHKW_max=1000, opt_BHKW_Speicher_min=0, 
-                          opt_BHKW_Speicher_max=100)
+    CHP_1 = CHP(
+        name="BHKW_1", 
+        th_Leistung_kW=100,  # Updated parameter name
+        spez_Investitionskosten_GBHKW=1500, 
+        spez_Investitionskosten_HBHKW=1850, 
+        el_Wirkungsgrad=0.33, 
+        KWK_Wirkungsgrad=0.9, 
+        min_Teillast=0.7, 
+        speicher_aktiv=False, 
+        Speicher_Volumen_BHKW=20, 
+        T_vorlauf=90, 
+        T_ruecklauf=60, 
+        initial_fill=0.0, 
+        min_fill=0.2, 
+        max_fill=0.8, 
+        spez_Investitionskosten_Speicher=750, 
+        active=True, 
+        opt_BHKW_min=0, 
+        opt_BHKW_max=1000, 
+        opt_BHKW_Speicher_min=0, 
+        opt_BHKW_Speicher_max=100
+    )
 
-    riverHeatPump = base_heat_pumps.WasteHeatPump(name="Abwärme_1", Kühlleistung_Abwärme=50, Temperatur_Abwärme=30, spez_Investitionskosten_Abwärme=500, 
-                                             spezifische_Investitionskosten_WP=1000, min_Teillast=0.2)
+    CHP_storage = CHP(
+        name="BHKW_2", 
+        th_Leistung_kW=100,  # Updated parameter name
+        spez_Investitionskosten_GBHKW=1500, 
+        spez_Investitionskosten_HBHKW=1850, 
+        el_Wirkungsgrad=0.33, 
+        KWK_Wirkungsgrad=0.9, 
+        min_Teillast=0.7, 
+        speicher_aktiv=True,  # Updated: with storage
+        Speicher_Volumen_BHKW=20, 
+        T_vorlauf=90, 
+        T_ruecklauf=60, 
+        initial_fill=0.0, 
+        min_fill=0.2, 
+        max_fill=0.8, 
+        spez_Investitionskosten_Speicher=750, 
+        active=True, 
+        opt_BHKW_min=0, 
+        opt_BHKW_max=1000, 
+        opt_BHKW_Speicher_min=0, 
+        opt_BHKW_Speicher_max=100
+    )
 
-    riverHeatPump = base_heat_pumps.RiverHeatPump(name="Flusswasser_1", Wärmeleistung_FW_WP=200, Temperatur_FW_WP=10, dT=0, spez_Investitionskosten_Flusswasser=1000, 
-                                             spezifische_Investitionskosten_WP=1000, min_Teillast=0.2)
+    # UPDATED: Heat pump definitions matching 09 example
+    wasteHeatPump = WasteHeatPump(
+        name="Abwärmepumpe", 
+        Kühlleistung_Abwärme=50, 
+        Temperatur_Abwärme=30, 
+        spez_Investitionskosten_Abwärme=500, 
+        spezifische_Investitionskosten_WP=1000, 
+        min_Teillast=0.2
+    )
 
-    geothermal_heat_pump = base_heat_pumps.Geothermal(name="Geothermie_1", Fläche=200, Bohrtiefe=100, Temperatur_Geothermie=10, spez_Bohrkosten=100, spez_Entzugsleistung=50,
-                                                Vollbenutzungsstunden=2400, Abstand_Sonden=10, spezifische_Investitionskosten_WP=1000, min_Teillast=0.2)
-    
-    # not implemented yet
-    #aqva_heat = heat_pumps.AqvaHeat(name="AqvaHeat_1", nominal_power=100, temperature_difference=0)
-    #test_aqva_heat(Last_L, duration, Strompreis, q, r, T, BEW, stundensatz, VLT_L, COP_data, aqva_heat)  
+    riverHeatPump = RiverHeatPump(
+        name="Flusswärmepumpe", 
+        Wärmeleistung_FW_WP=200, 
+        Temperatur_FW_WP=10, 
+        dT=0, 
+        spez_Investitionskosten_Flusswasser=1000, 
+        spezifische_Investitionskosten_WP=1000, 
+        min_Teillast=0.2
+    )
 
-    solarThermal = solar_thermal.SolarThermal(name="Solarthermie_1", bruttofläche_STA=200, vs=20, Typ="Vakuumröhrenkollektor", kosten_speicher_spez=750, kosten_fk_spez=430, kosten_vrk_spez=590, 
-                                            Tsmax=90, Longitude=-14.4222, STD_Longitude=-15, Latitude=51.1676, East_West_collector_azimuth_angle=0, Collector_tilt_angle=36, Tm_rl=60, 
-                                            Qsa=0, Vorwärmung_K=8, DT_WT_Solar_K=5, DT_WT_Netz_K=5, opt_volume_min=0, opt_volume_max=200, opt_area_min=0, opt_area_max=2000)
+    geothermal_heat_pump = Geothermal(
+        name="Geothermie", 
+        Fläche=200, 
+        Bohrtiefe=100, 
+        Temperatur_Geothermie=10, 
+        spez_Bohrkosten=100, 
+        spez_Entzugsleistung=50,
+        Vollbenutzungsstunden=2400, 
+        Abstand_Sonden=10, 
+        spezifische_Investitionskosten_WP=1000, 
+        min_Teillast=0.2
+    )
 
-    tech_objects = [CHP_storage, bBoiler, gBoiler]
+    solarThermal = SolarThermal(
+        name="Solarthermie", 
+        bruttofläche_STA=200, 
+        vs=20, 
+        Typ="Vakuumröhrenkollektor", 
+        kosten_speicher_spez=750, 
+        kosten_fk_spez=430, 
+        kosten_vrk_spez=590, 
+        Tsmax=90, 
+        Longitude=-14.4222, 
+        STD_Longitude=-15, 
+        Latitude=51.1676, 
+        East_West_collector_azimuth_angle=0, 
+        Collector_tilt_angle=36, 
+        Tm_rl=60, 
+        Qsa=0, 
+        Vorwärmung_K=8, 
+        DT_WT_Solar_K=5, 
+        DT_WT_Netz_K=5, 
+        opt_volume_min=0, 
+        opt_volume_max=200, 
+        opt_area_min=0, 
+        opt_area_max=2000
+    )
 
-    # Angaben zum zu betrchtende Zeitraum
-    # Erstelle ein Array mit stündlichen Zeitwerten für ein Jahr
+    # UPDATED: Technology selection - mix of different technologies
+    tech_objects = [
+        solarThermal,  # Solar thermal
+        CHP_1,      # CHP with storage
+        gBoiler,          # Gas boiler as backup
+    ]
+
+    # Time steps for simulation
     start_date = np.datetime64('2019-01-01')
-    end_date = np.datetime64('2020-01-01', 'D')  # Enddatum ist exklusiv, daher 'D' für Tage
-
-    # Erstelle das Array mit stündlichen Zeitwerten für ein Jahr
+    end_date = np.datetime64('2020-01-01', 'D')
     time_steps = np.arange(start_date, end_date, dtype='datetime64[h]')
 
+    # UPDATED: Economic parameters matching 09 example
     economic_parameters = {
-                "gas_price": gas_price,
-                "electricity_price": electricity_price,
-                "wood_price": wood_price,
-                "capital_interest_rate": q,
-                "inflation_rate": r,
-                "time_period": T,
-                "hourly_rate": hourly_rate,
-                "subsidy_eligibility": BEW
-            }
-
-    weights = {
-        "WGK_Gesamt": 1.0,
-        "specific_emissions_Gesamt": 0.0,
-        "primärenergiefaktor_Gesamt": 0.0
+        "gas_price": gas_price,
+        "electricity_price": electricity_price,
+        "wood_price": wood_price,
+        "capital_interest_rate": q,
+        "inflation_rate": r,
+        "time_period": T,
+        "hourly_rate": hourly_rate,
+        "subsidy_eligibility": BEW
     }
 
-    energy_system = energy_system.EnergySystem(
-                time_steps=time_steps,
-                load_profile=Last_L,
-                VLT_L=VLT_L,
-                RLT_L=RLT_L,
-                TRY_data=TRY_data,
-                COP_data=COP_data,
-                economic_parameters=economic_parameters,
-            )
+    # Optimization weights
+    weights = {
+        "WGK_Gesamt": 1.0,                    # Focus on cost optimization
+        "specific_emissions_Gesamt": 0.1,     # Small weight for emissions
+        "primärenergiefaktor_Gesamt": 0.0     # No weight for primary energy
+    }
 
-        # Add technologies to the system
+    # Create energy system
+    energy_system = EnergySystem(
+        time_steps=time_steps,
+        load_profile=Last_L,
+        VLT_L=VLT_L,
+        RLT_L=RLT_L,
+        TRY_data=TRY_data,
+        COP_data=COP_data,
+        economic_parameters=economic_parameters,
+    )
+
+    # Add technologies to the system
     for tech in tech_objects:
         energy_system.add_technology(tech)
 
-    # Calculate the energy mix
+    # Calculate the initial energy mix
     system_results = energy_system.calculate_mix()
-    print(f"Techs: {system_results['techs']}")
-    print(f"Energy mix: {system_results['Anteile']}")
-    print(f"WGK: {system_results['WGK_Gesamt']}")
+    print("=== Initial Energy Mix Results ===")
+    print(f"Technologies: {system_results['techs']}")
+    print(f"Energy mix shares: {system_results['Anteile']}")
+    print(f"Heat generation costs: {system_results['WGK_Gesamt']:.2f} €/MWh")
+    print(f"Specific emissions: {system_results.get('specific_emissions_Gesamt', 'N/A'):.3f} tCO2/MWh")
+    print(f"Primary energy factor: {system_results.get('primärenergiefaktor_Gesamt', 'N/A'):.3f}")
 
     if plot:
-        energy_system.plot_stack_plot(plt.figure())
-        energy_system.plot_pie_chart(plt.figure())
+        print("\nGenerating initial plots...")
+        fig1 = plt.figure(figsize=(12, 8))
+        energy_system.plot_stack_plot(fig1)
+        fig1.suptitle('Initial Energy Mix - Stack Plot')
+        
+        fig2 = plt.figure(figsize=(10, 8))
+        energy_system.plot_pie_chart(fig2)
+        fig2.suptitle('Initial Energy Mix - Pie Chart')
 
-    # Perform optimization if needed
+    # Perform optimization if requested
     if optimize:
+        print(f"\n=== Starting Optimization with {opt_method} ===")
+        
         if opt_method == "SLSQP":
-            print("Optimizing mix with SLSQP")
+            print("Optimizing mix with SLSQP...")
             optimized_energy_system = energy_system.optimize_mix(weights, num_restarts=5)
-        elif opt_method == "MILP":
-            print("Optimizing mix with MILP")
-            optimized_energy_system = energy_system.optimize_milp(weights, num_restarts=5)
-        print("Optimization done")
+        else:
+            print(f"Unknown optimization method: {opt_method}")
+            return
+            
+        print("Optimization completed!")
 
-        # Calculate the energy mix
+        # Calculate the optimized energy mix
         optimized_system_results = optimized_energy_system.calculate_mix()
-        print(f"Optimized Techs: {optimized_system_results['techs']}")
-        print(f"Optimized energy mix: {optimized_system_results['Anteile']}")
-        print(f"Optimized WGK: {optimized_system_results['WGK_Gesamt']}")
+        print("\n=== Optimized Energy Mix Results ===")
+        print(f"Technologies: {optimized_system_results['techs']}")
+        print(f"Energy mix shares: {optimized_system_results['Anteile']}")
+        print(f"Heat generation costs: {optimized_system_results['WGK_Gesamt']:.2f} €/MWh")
+        print(f"Specific emissions: {optimized_system_results.get('specific_emissions_Gesamt', 'N/A'):.3f} tCO2/MWh")
+        print(f"Primary energy factor: {optimized_system_results.get('primärenergiefaktor_Gesamt', 'N/A'):.3f}")
+
+        # Compare results
+        print("\n=== Optimization Comparison ===")
+        initial_cost = system_results['WGK_Gesamt']
+        optimized_cost = optimized_system_results['WGK_Gesamt']
+        cost_savings = initial_cost - optimized_cost
+        cost_savings_percent = (cost_savings / initial_cost) * 100
+
+        print(f"Initial costs: {initial_cost:.2f} €/MWh")
+        print(f"Optimized costs: {optimized_cost:.2f} €/MWh")
+        print(f"Cost savings: {cost_savings:.2f} €/MWh ({cost_savings_percent:.1f}%)")
 
         if plot:
-            optimized_energy_system.plot_stack_plot(plt.figure())
-            optimized_energy_system.plot_pie_chart(plt.figure())
+            print("\nGenerating optimized plots...")
+            fig3 = plt.figure(figsize=(12, 8))
+            optimized_energy_system.plot_stack_plot(fig3)
+            fig3.suptitle('Optimized Energy Mix - Stack Plot')
+            
+            fig4 = plt.figure(figsize=(10, 8))
+            optimized_energy_system.plot_pie_chart(fig4)
+            fig4.suptitle('Optimized Energy Mix - Pie Chart')
+
+    print("\n=== Analysis Complete ===")
+    return energy_system
 
 if __name__ == "__main__":
-    test_berechnung_erzeugermix(optimize=True, plot=True, opt_method="SLSQP")
+    print("Starting Heat Generation Optimization Example")
+    print("=" * 50)
+    
+    # Test with different optimization methods
+    print("\n1. Testing without optimization:")
+    energy_system_base = test_berechnung_erzeugermix(optimize=False, plot=True)
+    
+    print("\n2. Testing with SLSQP optimization:")
+    energy_system_slsqp = test_berechnung_erzeugermix(optimize=True, plot=True, opt_method="SLSQP")
 
+    print("\nShowing all plots...")
     plt.show()

src/districtheatingsim/net_simulation_pandapipes/utilities.py
@@ -176,11 +176,12 @@ def create_controllers(net, qext_w: np.ndarray, supply_temperature_heat_generato
     return_temperature_heat_consumer : np.ndarray
         Return temperature setpoints for heat consumers [°C].
         Array length must match the number of heat consumers.
-    secondary_producers : Optional[List[Dict[str, Any]]], optional
-        List of secondary producer configurations, each containing:
-        
-        - **"index"** (int) : Producer identification index
-        - **"mass_flow"** (float) : Design mass flow rate [kg/s]
+    secondary_producers : Optional[List[SecondaryProducer]], optional
+    List of secondary producer objects, each containing:
+
+        - **index** (int) : Producer identification index
+        - **mass_flow** (float) : Design mass flow rate [kg/s]
+        - **load_percentage** (float) : Load percentage of total demand [%]
 
     Returns
     -------
@@ -279,23 +280,26 @@ def create_controllers(net, qext_w: np.ndarray, supply_temperature_heat_generato
     # Secondary producer controllers
     if secondary_producers:
         for producer in secondary_producers:
-            mass_flow = producer.get("mass_flow", 0)
+            # KORRIGIERT: Verwende Attribut-Zugriff statt Dictionary-Zugriff
+            mass_flow = producer.mass_flow if hasattr(producer, 'mass_flow') else 0
+            producer_index = producer.index if hasattr(producer, 'index') else 0
             
             # Mass flow controller for circulation pump
-            placeholder_df = pd.DataFrame({f'mdot_flow_kg_per_s_{producer["index"]}': [mass_flow]})
+            placeholder_df = pd.DataFrame({f'mdot_flow_kg_per_s_{producer_index}': [mass_flow]})
             placeholder_data_source = DFData(placeholder_df)
             ConstControl(net, element='circ_pump_mass', variable='mdot_flow_kg_per_s', element_index=0,
-                        data_source=placeholder_data_source, profile_name=f'mdot_flow_kg_per_s_{producer["index"]}')
+                        data_source=placeholder_data_source, profile_name=f'mdot_flow_kg_per_s_{producer_index}')
 
             # Flow control for producer
-            placeholder_df_flow = pd.DataFrame({f'controlled_mdot_kg_per_s_{producer["index"]}': [mass_flow]})
+            placeholder_df_flow = pd.DataFrame({f'controlled_mdot_kg_per_s_{producer_index}': [mass_flow]})
             placeholder_data_source_flow = DFData(placeholder_df_flow)
             ConstControl(net, element='flow_control', variable='controlled_mdot_kg_per_s', element_index=0,
-                        data_source=placeholder_data_source_flow, profile_name=f'controlled_mdot_kg_per_s_{producer["index"]}')
+                        data_source=placeholder_data_source_flow, profile_name=f'controlled_mdot_kg_per_s_{producer_index}')
 
             # Supply temperature for secondary producers
             ConstControl(net, element='circ_pump_mass', variable='t_flow_k', element_index=0, 
                         data_source=placeholder_data_source_supply_temp, profile_name='supply_temperature')
+
 
     # System pressure management controller
     dp_controller = BadPointPressureLiftController(net)